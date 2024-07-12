b4de0b1ee67621cc362e8eaf286cb986fe3a6d61
Gogy <thegogy@proton.me>
2024-07-12 03:35:08 +0100

    added tests from Vajolet

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,12 @@ int main (int argc, char *argv[]) {
 
     init_bbs();
     Position pos;
-    pos.setFromFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
+    // pos.setFromFEN("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
 
     print_position(pos);
-    // testFromFile("tests/perft_standard.txt");
+    testFromFile("tests/perft_vajolet.txt");
 
-    perft(pos, 7);
+    // perft(pos, 7);
 
     return 0;
 }
