46ebdb1e7a2511220e6c288be1b5f4084442c429
Gogy <thegogy@proton.me>
2024-07-15 23:39:49 +0100

    Removed split counting for leaf and inner nodes
    This seems to have a noticeable impact on perft performance, so has been
    removed.

--- src/perft.cpp
+++ src/perft.cpp
@@ -11,90 +11,70 @@
 
 using std::ifstream;
 
-/*
- * Runs a perft test on the given position.
- *
- * Returns: 
- *  - total_leaf: The number of leaf nodes (moves generated)
- *  - total_node: The number of nodes that have been expanded (number of times enumerateLegalMoves has been run)
- */
-template <bool Div, Side Me>
-std::tuple<size_t, size_t> perft(Position &pos, int depth) {
-  size_t total_leaf = 0;
-  size_t total_node = 0;
+template <bool Div, Side Me> size_t perft(Position &pos, int depth) {
+  size_t total = 0;
   MoveList moves;
 
   if (!Div && depth <= 1) {
     enumerateLegalMoves<Me>(pos, [&](Move m) {
-      ++total_leaf;
+      ++total;
       return true;
     });
 
-    ++total_node;
-
-    return std::tuple<size_t, size_t>(total_leaf, total_node);
+    return total;
   }
 
   enumerateLegalMoves<Me>(pos, [&](Move move) {
-      std::tuple<size_t, size_t> n {0, 0};
+    size_t n = 0;
 
     if (Div && depth == 1) {
-     ++std::get<0>(n);
+      n = 1;
     } else {
       pos.doMove<Me>(move);
-      if (depth == 1) {
-        std::get<0>(n) = 1;
-      } else {
-        n = perft<false, ~Me>(pos, depth - 1);
-      }
-
+      n = (depth == 1 ? 1 : perft<false, ~Me>(pos, depth - 1));
       pos.undoMove<Me>(move);
     }
 
-    total_leaf += std::get<0>(n);
-    total_node += std::get<1>(n);
+    total += n;
 
-    if (Div && std::get<0>(n) > 0)
-      std::cout << Uci::formatMove(move) << ": " << std::get<0>(n) << std::endl;
+    if (Div && n > 0)
+      std::cout << Uci::formatMove(move) << ": " << n << std::endl;
 
     return true;
   });
 
-  return std::tuple<size_t, size_t>(total_leaf, total_node);
+  return total;
 }
 
-template std::tuple<size_t, size_t> perft<true, WHITE>(Position &pos, int depth);
-template std::tuple<size_t, size_t> perft<false, WHITE>(Position &pos, int depth);
-template std::tuple<size_t, size_t> perft<true, BLACK>(Position &pos, int depth);
-template std::tuple<size_t, size_t> perft<false, BLACK>(Position &pos, int depth);
+template size_t perft<true, WHITE>(Position &pos, int depth);
+template size_t perft<false, WHITE>(Position &pos, int depth);
+template size_t perft<true, BLACK>(Position &pos, int depth);
+template size_t perft<false, BLACK>(Position &pos, int depth);
 
 template<bool Div>
-std::tuple<size_t, size_t> perft(Position &pos, int depth) {
+size_t perft(Position &pos, int depth) {
     return pos.getSideToMove() == WHITE ? perft<Div, WHITE>(pos, depth) : perft<Div, BLACK>(pos, depth);
 }
 
-template std::tuple<size_t, size_t> perft<true>(Position &pos, int depth);
-template std::tuple<size_t, size_t> perft<false>(Position &pos, int depth);
+template size_t perft<true>(Position &pos, int depth);
+template size_t perft<false>(Position &pos, int depth);
 
 void perft(Position &pos, int depth) {
-    size_t total_leaf, total_node;
     auto begin = std::chrono::high_resolution_clock::now();
-    std::tie(total_leaf, total_node) = perft<true>(pos, depth);
+    size_t n = perft<true>(pos, depth);
     auto end = std::chrono::high_resolution_clock::now();
 
     auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
     std::cout << std::endl;
-    std::cout << "Inner Nodes:      " << total_node << std::endl;
-    std::cout << "Leaf nodes:       " << total_leaf << std::endl;
-	  std::cout << "Time:             " << elapsed << "ms" << std::endl;
-	  std::cout << "Positions / sec:  " << size_t(total_node * 1000 / elapsed) << std::endl;
-	  std::cout << "Moves / sec:      " << size_t(total_leaf * 1000 / elapsed) << std::endl;
+    std::cout << "Nodes:    " << n << std::endl;
+	std::cout << "Time:     " << elapsed << "ms" << std::endl;
+	std::cout << "NPS:      " << size_t(n * 1000 / elapsed) << std::endl;
 }
 
 bool runTest(const std::string &fen, int depth, U64 expected_nodes) {
     Position pos;
     pos.setFromFEN(fen);
-    size_t nodes = std::get<0>(perft<false>(pos, depth));
+    U64 nodes = perft<false>(pos, depth);
     if (nodes == expected_nodes) {
         std::cout << "[PASS] " << fen << std::endl;
         return true;
@@ -150,4 +130,3 @@ void testFromFile(const std::string &filename) {
     std::cout << "Total tests:      " << total_tests << std::endl;
     std::cout << "Tests passed:     " << tests_passed << std::endl;
 }
-
